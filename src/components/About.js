746eae8ea4e4db1f8b19eb76e77046ff4a7db75c
unknown <hd1@somaiya.edu>
2024-06-05 16:26:06 +0530

    about us basic

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,12 @@ import './styles/About.css'
 export default function About() {
   return (
     <div className='main-bg-about-us'>
-      <div className='image-left'>
-        <img className='image-yoga' alt="Yoga"/>
-      </div>
-      <div className='content-right'>
-        <div className='intro-container'>
+      <div className='image-left'></div>
+      <div  className='content-right'>
             <h2>Introduction</h2>
-            <p>hbeksrurshu ffh fh rufhrwhf rh furh furwh fh wrfh rwofhrrf hrwh rh wrh rhw rh huohor hwrf hrfh r hhurh ure htuerh uerh u herugh eugh eug her hguer her hgueg eh herh erhhu hug htukg kegh ke hgkrh grh kgrh gh g.</p>
-        </div>
-        <div className='ourstory-container'>
+            <p>hbeksrurshu ffh fh rufhrwhf rh furh furwh fh jnenjnjkvnevo eoc hoc heio coti joite oi oie jioej ij ipej igpjiej giej i jeigj 4ti jt4ij tigj tigj 4ti gjoit joi4 oi4h goi4 gio4 hli hgo hgith ocg t4hocgh t4ogch 4ogh 4uog h4tuohe hg4t hgwrfh rwofhrrf hrwh rh wrh rhw rh huohor hwrf hrfh r hhurh ure htuerh uerh u herugh eugh eug her hguer her hgueg eh herh erhhu hug htukg kegh ke hgkrh grh kgrh gh g.</p>
             <h2>Our Story</h2>
-            <p>hbeksrurshu ffh fh rufhrwhf rh furh furwh fh wrfh rwofhrrf hrwh rh wrh rhw rh huohor hwrf hrfh r hhurh ure htuerh uerh u herugh eugh eug her hguer her hgueg eh herh erhhu hug htukg kegh ke hgkrh grh kgrh gh g.</p>
-        </div>
+            <p>hbeksrurshu ffh fh rufhrwhf rh furh furwh fh wrfh rwofhrrf hrwh rh wrh rhw rh huohor hwrf hrfh r hhurh ure htuerh uerh u herugh eugh eug her hguer henenlijgie jgioej go jigj igj lej liej l lj li gijgeh  hegj eijgioj4t igjlg 4lg 4hg4 g 4tgjbt4g4t gt4 gt4 g4t g4tbgt4kg 4r hgueg eh herh erhhu hug htukg kegh ke hgkrh grh kgrh gh g.</p>
       </div>
     </div>
   )
